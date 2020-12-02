d28fd7e9e839eb1fc2969d742e0080a8720cb430
Andrea <cappelli.andrea.1998@gmail.com>
2020-12-02 15:09:49 +0100

    Vedi un po se riesci risolvere i problemi che l'app crasha

--- app/src/main/java/com/example/genovagreen/MainActivity.java
+++ app/src/main/java/com/example/genovagreen/MainActivity.java
@@ -1,100 +1,70 @@
 package com.example.genovagreen;
 
-import android.app.ActionBar;
-import android.support.v7.app.ActionBar;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Menu;
-import android.widget.Toast;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout dl;
-    private ActionBarDrawerToggle abdt;
-
-    private AppBarConfiguration mAppBarConfiguration;
+    DrawerLayout dl;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+        dl=findViewById(R.id.dl);
+    }
+    public void ClickMenu(View view){
+        openDrawer(dl);
+    }
 
+    public static void openDrawer(DrawerLayout dl) {
+        dl.openDrawer(GravityCompat.START);
+    }
+    public void ClickLogo(View view){
+        closeDrawer(dl);
+    }
 
-        dl=(DrawerLayout)findViewById(R.id.dl) ;
-        abdt=new ActionBarDrawerToggle(this,dl, R.string.Open, R.string.Close);
-        abdt.setDrawerIndicatorEnabled(true);
-        dl.addDrawerListener(abdt);
-        abdt.syncState();
+    public void closeDrawer(DrawerLayout dl) {
+        if(dl.isDrawerOpen(GravityCompat.START)) {
+            dl.closeDrawer(GravityCompat.START);
+        }
+    }
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+    public void ClickHome(View view){
+        recreate();
+    }
+    public void ClickDoveLoButto(View view){
+        redirectActivity(this,Dove_lo_butto.class);
+    }
+    public void ClickPericolosi(View view){
+        redirectActivity(this,Pericolosi.class);
+    }
+    public void ClickSpedizioni(View view){
+        redirectActivity(this,Spedizioni.class);
+    }
+    public void ClickImpostazioni(View view){
+        redirectActivity(this,Impostazioni.class);
+    }
+    public void ClickContatti(View view){
+        redirectActivity(this,Contatti.class);
+    }
 
-        final NavigationView nav_view=(NavigationView)findViewById(R.id.nav_view);
-        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                int id= menuItem.getItemId();
-                        if(id== R.id.nav_home)
-                        {
-                            Toast.makeText(MainActivity.this,"Home", Toast.LENGTH_SHORT).show();
-                        }
-                else if(id== R.id.nav_butto)
-                {
-                    Toast.makeText(MainActivity.this,"Dove lo butto?", Toast.LENGTH_SHORT).show();
-                }
-                else if(id== R.id.nav_pericolosi)
-                {
-                    Toast.makeText(MainActivity.this,"Rifiuti pericolosi", Toast.LENGTH_SHORT).show();
-                }
-                else if(id== R.id.nav_spedizioni)
-                {
-                    Toast.makeText(MainActivity.this,"Spedizioni", Toast.LENGTH_SHORT).show();
-                }
-                else if(id== R.id.nav_impostazioni)
-                {
-                    Toast.makeText(MainActivity.this,"Impostazioni", Toast.LENGTH_SHORT).show();
-                }
-                else if(id== R.id.nav_contatti)
-                {
-                    Toast.makeText(MainActivity.this,"Contatti", Toast.LENGTH_SHORT).show();
-                }
-                return false;
-            }
-        });
-
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent=new Intent(activity,aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        return abdt.onOptionsItemSelected(item)||super.onOptionsItemSelected(item);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(dl);
     }
 }
---
+++ app/src/main/java/com/example/genovagreen/Spedizioni.java
@@ -0,0 +1,14 @@
+package com.example.genovagreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Spedizioni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spedizioni);
+    }
+}
